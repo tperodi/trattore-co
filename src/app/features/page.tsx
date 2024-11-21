c365d3620a901cf6fa106c5d7d8cf326ba56d598
tperodi <thomas.perodi.studenti@isii.it>
2024-11-21 11:10:11 +0100

    First commit

---
+++ src/app/features/page.tsx
@@ -0,0 +1,15 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+export default function FeaturesPage() {
+  return (
+    <Container>
+      <Typography variant="h3" component="h1" gutterBottom>
+        Caratteristiche principali
+      </Typography>
+      <Typography>
+        Elenca le funzionalità qui.
+      </Typography>
+    </Container>
+  );
+}
